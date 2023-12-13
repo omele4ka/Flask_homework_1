4a0abb128c057534e45b81651c53853660816601
omele4ka <omele4ka@inbox.ru>
2023-12-13 14:30:37 -0500

    Created Templates folder, News file. Task is done

--- app_007.py
+++ app_007.py
@@ -5,3 +5,26 @@
 # краткое описание и дату публикации.
 # Данные о новостях должны быть переданы в шаблон через
 # контекст.
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def news_block():
+    context = [
+        {'title' : 'Какая-то новость',
+         'text' : 'варырмымжымжтымыыом',
+         'date' : '12.12.2023'},
+        {'title' : 'Еще какая-то новость',
+         'text' : 'варырмымжымжтымыыом ымгрышм',
+         'date' : '12.12.2023'},
+        {'title' : 'Супер важная новость',
+         'text' : 'варырмымжымжтымыыом',
+         'date' : '12.12.2023'}
+    ]
+
+    return render_template('news.html', context=context)
+
+if __name__== '__main__':
+    app.run(debug=True)
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from homework_1.app_007 import app
+
+
+if __name__ == '__main__':
+    app.run()
